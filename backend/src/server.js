b8a84bcb5def9cb71bdd369bc435b4cbce5c2d25
ben <blin29@bu.edu>
2026-04-27 17:23:42 -0400

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,8 +21,8 @@ app.use(
   cors({
     origin: [
       FRONTEND_URL,
-      "http://localhost:5500",
-      "http://127.0.0.1:5500",
+      "http://localhost:5000",
+      "http://127.0.0.1:5000",
       "http://localhost:3000",
       "http://localhost:5173"
     ],

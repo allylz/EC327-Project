34deba44b85046d6d460c5ca3e6a8b4b422086d6
ben <blin29@bu.edu>
2026-04-28 03:29:16 -0400

    MAKE SQUARES

--- test.py
+++ test.py
@@ -4467,5 +4467,6 @@ BASE_HTML = BASE_HTML.replace("</body>", r'''
 </body>''')
 
 if __name__ == "__main__":
+    print("version: 29")
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", debug=True, port=port)
